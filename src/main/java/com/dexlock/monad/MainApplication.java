4f2da25d3af84d9af12888ae0be88c66c904fae5
Abhijith K S <abhijithks6400@gmail.com>
2019-11-25 19:31:08 +0530

    Created Seperate Class for CompletableFuture

--- src/main/java/com/dexlock/monad/MainApplication.java
+++ src/main/java/com/dexlock/monad/MainApplication.java
@@ -4,44 +4,8 @@ import java.util.concurrent.CompletableFuture;
 
 public class MainApplication {
     public static void main(String[] args) {
+        CompletableFutureExample completableFutureExample = new CompletableFutureExample();
         String url = "https://facebook.com";
-        checkUrl(url);
-    }
-
-    private static void checkUrl(final String url) {
-        Helper helper = new Helper();
-
-        CompletableFuture<Boolean> validateUrl = CompletableFuture.supplyAsync(() -> {
-            if ((helper.validateUrl(url))) {
-                return true;
-            }
-            throw new RuntimeException("Invalid URL");
-        }).exceptionally(throwable -> {
-            System.out.println(throwable);
-            return null;
-        });
-
-        CompletableFuture<String> sendGetUrl = validateUrl.thenApplyAsync(Boolean -> {
-            try {
-                String result = helper.getUrl(url);
-                helper.close();
-                return result;
-            } catch (Exception e) {
-                throw new RuntimeException("URL doesn't exist");
-            }
-        }).exceptionally(throwable -> {
-            System.out.println(throwable);
-            return null;
-        });
-
-        CompletableFuture<Boolean> printUrl = sendGetUrl.thenApply(urlBody -> {
-            System.out.println(urlBody);
-            return true;
-        }).exceptionally(throwable -> {
-            System.out.println("No URL Body");
-            return false;
-        });
-
-        printUrl.join();
+        completableFutureExample.checkUrlString(url);
     }
 }
